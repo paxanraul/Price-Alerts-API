caab82e9dbc131714fe9e71899a5a4d36751f61a
paxanraul <raul20081033@gmail.com>
2026-07-03 14:52:03 +0300

    add: model usre

---
+++ app/models/user.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from sqlalchemy import String, func
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+
+from app.db.base import Base
+
+
+class User(Base):
+	__tablename__ = "users"
+
+	id: Mapped[int] = mapped_column(primary_key=True)
+	email: Mapped[str] = mapped_column(String(100))
+	hashed_password: Mapped[str] = mapped_column(nullable=False)
+	is_active: Mapped[bool] = mapped_column(default=True)
+	created_at: Mapped[datetime] = mapped_column(default=func.now())
+	updated_at: Mapped[datetime] = mapped_column(default=func.now(), onupdate=func.now())
