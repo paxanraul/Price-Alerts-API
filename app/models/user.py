1de38ca5bdd9b392fd5e223aae52a78a51fb013a
paxanraul <raul20081033@gmail.com>
2026-07-06 13:52:55 +0300

    add: unique in email

--- app/models/user.py
+++ app/models/user.py
@@ -9,7 +9,7 @@ class User(Base):
 	__tablename__ = "users"
 
 	id: Mapped[int] = mapped_column(primary_key=True)
-	email: Mapped[str] = mapped_column(String(100))
+	email: Mapped[str] = mapped_column(String(100), unique=True)
 	hashed_password: Mapped[str] = mapped_column(nullable=False)
 	is_active: Mapped[bool] = mapped_column(default=True)
 	created_at: Mapped[datetime] = mapped_column(default=func.now())
