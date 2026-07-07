71d139a48b9fb814b7ef683a8d829e6bc7ae4dee
paxanraul <raul20081033@gmail.com>
2026-07-07 17:55:13 +0300

    connect to router

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from sqlalchemy import text
+
 from app.db.base import engine
+from app.api.router import api_router
 
 app = FastAPI(title="Price Alerts")
 
-
+app.include_router(api_router)
 
 @app.get("/health")
 async def health():
