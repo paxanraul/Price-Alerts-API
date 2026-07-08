532a4dd15b0ce5571554d4dcb321135b4e4307d6
paxanraul <raul20081033@gmail.com>
2026-07-08 15:29:01 +0300

    connect prices router

--- app/api/router.py
+++ app/api/router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from app.api.v1 import auth
+from app.api.v1 import auth, prices
 
 api_router = APIRouter(prefix="/api/v1")
 api_router.include_router(auth.router)
+api_router.include_router(prices.router)
