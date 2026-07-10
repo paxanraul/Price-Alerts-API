85d36e49f5b6fa2145a76d1d61acbd38e0ae19d6
paxanraul <raul20081033@gmail.com>
2026-07-10 18:08:59 +0300

    import delivery

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -8,7 +8,7 @@ from alembic import context
 
 from app.core.config import settings
 from app.db.base import Base
-from app.models import user, alert, alert_trigger
+from app.models import user, alert, alert_trigger, delivery
 
 config = context.config
 
