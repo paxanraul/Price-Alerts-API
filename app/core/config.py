202a1f3ef875d7efe7a031320dd894be37827615
paxanraul <raul20081033@gmail.com>
2026-07-22 00:02:18 +0300

    add telegram notifications

--- app/core/config.py
+++ app/core/config.py
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
 	SECRET_KEY: str
 	ALGORITHM: str = "HS256"
 	ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+	BOT_TOKEN: str
 
 	model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
